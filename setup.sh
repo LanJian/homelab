436d0edead39aff30b4082be56c5a05cda2ca443
Jack Huang <jackhxs@gmail.com>
2023-07-22 22:44:20 -0700

    feat: add bootstrap script and fix cert setup

--- setup.sh
+++
@@ -1,7 +0,0 @@
-# WIP
-
-kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
-kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
-
-kubectl create secret generic -n cert-manager cloudflare-api-token-secret \
-  --from-literal=api-token=`pass -o cloudflare/cert-manager-api-token`
